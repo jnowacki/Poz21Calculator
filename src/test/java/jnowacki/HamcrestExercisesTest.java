4f23bcf99d27bc6d715a4be94e417ffab7e275bd
Jedrzej Nowacki <contact@jnowacki.pl>
2019-12-08 11:56:34 +0100

    Hamcrest feature matcher

--- src/test/java/jnowacki/HamcrestExercisesTest.java
+++ src/test/java/jnowacki/HamcrestExercisesTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -33,14 +34,42 @@ public class HamcrestExercisesTest {
     @Test
     public void doesArrayContainRightValues() {
 
+        Integer[] values = {5, 7, 2};
+
+        assertThat(values, arrayContaining(5, 7, 2));
     }
 
     @Test
     public void doesArrayContainRightValuesInAnyOrder() {
+
+        Integer[] values = {5, 7, 2};
+
+        assertThat(values, arrayContainingInAnyOrder(2, 5, 7));
     }
 
     @Test
     public void doesListContainAndHasRightLength() {
 
+        List<String> values = Arrays.asList("raz", "dwa", "trzy");
+
+        assertThat(values, hasItem("dwa"));
+        assertThat(values, hasSize(3));
+    }
+
+    @Test
+    public void shouldBeginWithA() {
+        String testedValue = "aString";
+
+        assertThat(testedValue, beginsWith(is('a')));
+    }
+
+    private static Matcher<String> beginsWith(Matcher<? super Character> matcher) {
+        return new FeatureMatcher<String, Character>(matcher,
+                "a String that begins with", "first letter") {
+            @Override
+            protected Character featureValueOf(String actual) {
+                return actual.charAt(0);
+            }
+        };
     }
 }
